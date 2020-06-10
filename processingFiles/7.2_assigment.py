8305cb7cfcaf102b98408df399d76f573a80ae5e
nassarnour <nassarnour98@gmail.com>
2020-06-09 21:25:08 -0400

    Fomatting & File Processing Assigments

---
+++ processingFiles/7.2_assigment.py
@@ -0,0 +1,33 @@
+#7.1 Write a program that prompts for a file name, 
+#then opens that file and reads through the file, 
+#and print the contents of the file in upper case. 
+#Use the file words.txt to produce the output below.
+#You can download the sample data at http://www.py4e.com/code3/words.txt
+
+fname= input('Enter file name: ')
+try:
+    fileHandler=open(fname)
+    numOfRows = 0
+    #numOfRowsf=float(numOfRows)
+
+    totalAmount=0
+   # totalAmountf=float(totalAmount)
+
+    tempValue=0
+   # tempValuef=float(tempValue)
+
+    average=0
+    #avaeragef=float(average)
+
+    for line in fileHandler:
+        line=line.rstrip()
+        if line.startswith("X-DSPAM-Confidence:"):
+            tempValue=(line[20:28]) #gets current number
+            totalAmount= float(totalAmount)+float(tempValue) #adds it to total
+            numOfRows= numOfRows+1 #count total
+            #divide by total
+    average=float(totalAmount)/float(numOfRows)
+    print('Average spam confidence:', average)
+except:
+    print('Could not open: ',fname)
+    quit()
